9e5f3598230e915ba4b4d3e18a4bfe975f8c090e
Yijie Zhu <yzhu156@ucsc.edu>
2022-05-29 19:02:21 -0700

    Including API Gateway URL for sending POST request

--- form.js
+++ form.js
@@ -2,44 +2,64 @@ function processForm(e)
 {
 	e.preventDefault()
 
-	var name = document.getElementById('name-input').value
+	const name = document.getElementById('name-input').value
 
 	if(name.length === 0){
 		alert("Please enter your name!")
 		return false
 	}
 
-	var subject = document.getElementById('subject-input').value
+	const subject = document.getElementById('subject-input').value
 
 	if(subject.length === 0){
 		alert("Please enter a subject!")
 		return false
 	}
 
-	var email = document.getElementById('email-input').value
+	const email = document.getElementById('email-input').value
 
 	if(email.length === 0){
 		alert("Please enter an email!")
 		return false
 	}
 
-	var email_tester = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/
+	const email_tester = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/
 
 	if(!email_tester.test(email)){
 		alert("Please enter a valid email address!")
 	}
 
-	var message = document.getElementById('message-input').value
+	const message = document.getElementById('message-input').value
 
     if(message.length === 0){
 		alert("Please enter a message!")
 		return false
 	}
 
-    console.log(name)
-	console.log(subject)
-	console.log(email)
-	console.log(message)
+	console.log("The form will be submitted!\n")
+	console.log("Name attribute is:" + name)
+	console.log("\nSubject attribute is:" + subject)
+	console.log("\nEmail attribute is:" + email)
+	console.log("\nMessage attribute is:" + message)
+
+	const api_url = "https://aun9wb8r9c.execute-api.us-east-1.amazonaws.com/Prod"
+
+	let data = {
+		name: name,
+		email: email,
+		message:message,
+		subject: subject
+	}
+
+	fetch(api_url, {
+		method: "POST",
+		body: JSON.stringify(data),
+		headers: {"Content-type": "application/json; charset=UTF-8"}
+	  })
+	  .then(response => response.json()) 
+	  .then(json => console.log("Fetch API json response:", json, "\n"))
+	  .catch(err => console.log("Error occurs when sending the Fetch API request", err, "\n"))
+
 
     alert('Contact form submitted successfully!')
 
@@ -49,7 +69,7 @@ function processForm(e)
 }
 
 
-var form = document.getElementById('contact-form')
+let form = document.getElementById('contact-form')
 if(form.attachEvent){
 	form.attachEvent("submit", processForm)
 } else {
